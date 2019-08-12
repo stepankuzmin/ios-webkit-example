86cd9ae101a73cab20beaff25715225799fc191e
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2019-08-12 10:12:57 +0300

    feat: add webView

--- ios-webkit-example/ViewController.swift
+++ ios-webkit-example/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://google.com")
+        webView.load(URLRequest(url: url!))
     }
 
 
